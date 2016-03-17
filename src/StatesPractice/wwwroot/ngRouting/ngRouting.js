3523f68ede2cec5f05dd82eb2cfe7c6984918d6d
Mason Matlock <mwilliam09@gmail.com>
2016-03-17 16:08:00 -0500

    Initial Commit: Created Project, Added Fortunes, Added Eric Quotes

---
+++ src/StatesPractice/wwwroot/ngRouting/ngRouting.js
@@ -0,0 +1,101 @@
+var NGRouting;
+(function (NGRouting) {
+    angular.module("NGRouting", ["ui.router"])
+        .config(function ($stateProvider, $locationProvider, $urlRouterProvider) {
+        $stateProvider
+            .state("Home", {
+            url: "/",
+            templateUrl: "/ngRouting/home.html"
+        }).state("Contact Us", {
+            url: "/contact",
+            templateUrl: "/ngRouting/contact.html"
+        }).state("About Me", {
+            url: "/about",
+            templateUrl: "/ngRouting/about.html"
+        }).state("Eric Quotes", {
+            url: "/eric",
+            templateUrl: "/ngRouting/ericQuotes.html",
+            controller: QuoteController,
+            controllerAs: "qtc"
+        }).state("Fortunes", {
+            url: "/fortunes",
+            templateUrl: "/ngRouting/fortunes.html",
+            controller: QuoteController,
+            controllerAs: "qtc"
+        });
+        $urlRouterProvider.otherwise("/");
+        $locationProvider.html5Mode(true);
+    });
+    var QuoteService = (function () {
+        function QuoteService() {
+            this.ericQuotes = [
+                '"You Firefox users just have so many problems..."',
+                '"Go character by character..."',
+                '"Oh God, oh God, shit, come back! ... Your track-pad is very sensitive..."',
+                '"How did we fix this last time again?"',
+                '"So I wrote a game engine in Groovy..."',
+                '"One time I wrote an assembly code generator..."',
+                '"Tattoo it to your ... the back of your hand. I would\'ve said forehead, but you couldn\'t exactly see it there."',
+                '"What are you doing? Something crazy, no doubt."',
+                '"Make sure they\'re strings...not variable references. ~ahem~"',
+                '"It\'s the Heisenbug. The bug only exists when you\'re not looking for it."',
+                '"Sweet!"',
+                '"Craft your burrito to contain all the data you need to know."',
+                '"But I\'m weird, so ... y\'know."',
+                '"It\'s like the DMV ... it just doesn\'t work."',
+                '"REPONSE!"',
+                '"Oh come ON!"',
+                '"Very disappointing."',
+                '"It\'s gonna look like crap anyways, \'cause my CSS is about ... here."',
+                '"What the fuck is haaaappening..."',
+                '"Modal modal modal modal modal modal modal."'
+            ];
+            this.fortunes = [
+                '"Your future contains murder."',
+                '"Your program will not compile."',
+                '"Help, I am trapped in a fortune cookie factory."',
+                '"Your future contains syntax errors. Everywhere."',
+                '" Abort, Retry, Fail?"',
+                '"The program will compile, but show nothing."',
+                '"Be careful of Off by One errors."',
+                '"Double check your typings."',
+                '"Git Repo Disaster."',
+                '"Your future contains Ruby."',
+                '"Your future contains ridiculous documentation."',
+                '"Your future contains JavaScript."',
+                '"I foresee Runtime Errors in your future."',
+                '"Be careful when coding arrays this time."',
+                '"RegEx..."',
+                '"Your future contains C#."',
+                '"Beware when handling error messages..."',
+                '"Watch out for Internet Explorer."',
+                '"TILT"',
+                '"You have been banished to the wastelands of PHP."'
+            ];
+        }
+        QuoteService.prototype.randomQuote = function (arr) {
+            var rand = Math.floor(Math.random() * arr.length);
+            return arr[rand];
+        };
+        return QuoteService;
+    }());
+    angular.module("NGRouting").service("quoteService", QuoteService);
+    var QuoteController = (function () {
+        function QuoteController(quoteService) {
+            this.quoteService = quoteService;
+            //add button that grabs a random, rather than on refresh.
+            //store which ones we've already seen, if it's going to pop it up, flip a coin about whether or not to show it.
+            this.quote = quoteService.randomQuote(quoteService.ericQuotes);
+            this.fortune = quoteService.randomQuote(quoteService.fortunes);
+        }
+        QuoteController.prototype.newFortune = function () {
+            this.fortune = this.quoteService.randomQuote(this.quoteService.fortunes);
+        };
+        QuoteController.prototype.newQuote = function () {
+            this.quote = this.quoteService.randomQuote(this.quoteService.ericQuotes);
+        };
+        return QuoteController;
+    }());
+    angular.module("NGRouting").controller("quoteController", QuoteController);
+})(NGRouting || (NGRouting = {}));
+//# sourceMappingURL=ngRouting.js.map
